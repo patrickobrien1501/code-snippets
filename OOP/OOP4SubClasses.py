ca3f57eace3517627ec163b8f7f8e51599901e1b
patrickobrien1501 <jpobrien@gmx.com>
2022-04-01 15:50:57 +0200

    upload oop4

--- OOP/OOP4SubClasses.py
+++ OOP/OOP4SubClasses.py
@@ -22,17 +22,55 @@ class Employee:
         # (1) using self here in front of raise_amount because it should only apply to the instance
         self.pay = int(self.pay * self.raise_amt)
 
-class Developer(Employee):
-    pass
+class Developer(Employee): # inherits attributes and methods from Employee
+    raise_amt = 1.10
 
-dev_1 = Developer('Corey', 'Schafer', 50000)
-dev_2 = Developer('Test', 'User', 60000)
+    # if we want to add an attribute that is specific to the developers subclass, such as a favourite 
+    # programming language, we give them their own init method
+    def __init__(self, first, last, pay, prog_lang):
+        # instead of copying the entire block from the parent class, we use super() to use the parent's init method
+        super().__init__(first, last, pay)
+        # Employee.__init__(first, last, pay) would do the same
+        self.prog_lang = prog_lang
+
+class Manager(Employee):
+
+    def __init__(self, first, last, pay, employees=None): # dont want to pass mutable objects here
+        super().__init__(first, last, pay)
+        if employees is None:
+            self.employees = []
+        else:
+            self.employees = employees
+
+    # add option to add or remove employees that the manager is supervising
+    def add_emp(self, emp):
+        if emp not in self.employees:
+            self.employees.append(emp)
+
+    def remove_emp(self, emp):
+        if emp in self.employees:
+            self.employees.remove(emp)
+
+    def print_emps(self):
+        for emp in self.employees:
+            print('-->', emp.fullname())
+
+
+dev_1 = Developer('Corey', 'Schafer', 50000, 'Python')
+dev_2 = Developer('Test', 'User', 60000, 'Java')
+
+mgr_1 = Manager('Sue', 'Smith', 90000, [dev_1])
+
+mgr_1.add_emp(dev_2)
+mgr_1.remove_emp(dev_1)
+mgr_1.print_emps()
 
 # print(dev_1.email)
-# print(dev_2.email)
+# print(dev_1.prog_lang)
 
-# print(help(Developer))
+print(mgr_1.email)
 
-print(dev_1.pay)
-dev_1.apply_raise()
-print(dev_1.pay)
+# Additional Python functions
+print(isinstance(mgr_1, Manager))
+
+print(issubclass(Developer, Employee))
